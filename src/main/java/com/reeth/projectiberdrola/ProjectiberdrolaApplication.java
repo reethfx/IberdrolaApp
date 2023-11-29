8522198f3508885e091da486474c83e4eea72634
reethfx <hectorblancog3@gmail.com>
2023-11-29 15:54:43 +0100

    Project structure and first commit

---
+++ src/main/java/com/reeth/projectiberdrola/ProjectiberdrolaApplication.java
@@ -0,0 +1,13 @@
+package com.reeth.projectiberdrola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectiberdrolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectiberdrolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reeth/projectiberdrola/ProjectiberdrolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reeth.projectiberdrola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectiberdrolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
